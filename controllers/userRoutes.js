c8113b6373745e85bbe96000fbcd4c001ab810a2
James Jackson <jjackson0228@gmail.com>
2024-08-28 21:37:41 -0400

    added partials folder modified all my handlebars. added login.handlebars with a form

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
 
+router.get('/login', (req, res) => {
+  res.render('pages/login', {
+    title: 'Login Page',
+  });
+});
+
 router.get('/', async (req, res) => {
   res.render('pages/homepage', {
     title: 'Home Page',
