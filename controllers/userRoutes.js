c3f31afd99c12b625b254b9b515cb3a2c69f68f9
James Jackson <jjackson0228@gmail.com>
2024-09-02 21:46:46 -0400

    added files and added to post.js and some other models and added to controllers

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
 
+//router.get "/login"finish router.get login
 router.get('/login', (req, res) => {
   res.render('pages/login', {
     title: 'Login Page',
@@ -7,10 +9,31 @@ router.get('/login', (req, res) => {
 });
 
 router.get('/', async (req, res) => {
-  res.render('pages/homepage', {
-    title: 'Home Page',
-    content: 'Welcome to the Foundation and Handlebars integration!',
-  });
+  //GET HTML route
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
+    console.log(posts);
+    res.render('homepage', {
+      posts,
+      loggedIn: req.session.loggedIn,
+      title: 'Home Page',
+      content: 'Welcome to the Foundation and Handlebars integration!',
+    });
+  } catch (e) {
+    res.status(500).json(e);
+  }
 });
+
+//add router.get "/dashboard"
+
+//router.get /post/:id"
 
 module.exports = router;
